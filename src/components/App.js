bb837f02835bb49bd69653b57a3c86f0fa59a2e7
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-10-18 20:02:19 -0400

    Setup inital app scaffold and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const PageOne = () => {
-  return <div>Page One</div>;
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <button>Click Me</button>
-    </div>
-  );
-};
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
