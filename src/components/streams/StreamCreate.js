5462ee1e417252f2b657bae4bf5e97c0ea5fd0a1
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-10-21 19:54:56 -0400

    Added in react-form and focused on form building for streams. About to progress into api and server hosting for RESTful records

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,67 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form"; // reduxForm is a lot like connect(). Adds a bunch to props
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  state = {};
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : "field"}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  // Due to the form below having an onSubmit prop with handleSubmit from redux forms, we get passed all the inputs' values on submission in an argument to the  helper below
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      // Since Redux Forms has the handleSubmit prop on our props, we use that and provide it with our helper function as an argument.
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+// Form values from the onSubmit will be passed in here too for validation. Rreturning an empty object says everything is OK
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must provide a Stream title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must provide a Stream description";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate: validate
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+// applyMiddleware and compose in this case is used for redux devtools chrome extension.
 import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+// Sets up the redux devtools chrome extension and adds it to our redux store
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
