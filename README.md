ba0a1156fd9cf29800112e112f35b81029274991
Benjamin Broestl <Bensgamingmail@yahoo.com>
2019-10-28 18:58:28 -0400

    Finished streaming lifecycle on player

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Description
 
+A video streaming app where you can use OBS to stream programming sessions.
+
 ### Tech Specifications
 
 - Built in React.JS & Create-React-App
@@ -12,8 +14,11 @@
   - Axios for backend API requests and CRUD actions.
   - Redux & Redux Thunk
   - Lodash for object manipulation
+  - Node Media Server for RTMP video streaming (FLV.js & NodeMediaServer)
 
 #### Notes
 
-To run JSON-Server, cd into /api folder and run `npm start`. JSON-Server runs at :3001 and <http://localhost:3001/streams>
-To run app, cd into /client folder and run `npm start`
+1. To run JSON-Server, cd into `/api` folder and run `npm start`. JSON-Server runs at :3001 and <http://localhost:3001/streams>
+2. To run app, cd into `/client` folder and run `npm start`
+3. To run media server for RTMP video streaming, cd into `/rtmpserver` and run `npm start`. NodeMediaServer runs on port :8000, RTMP on port :1935
+4. To stream from OBS, find the ID of your stream in the URL, for example the '6' in (/streams/6), open OBS, File -> Settings then Stream, in Service select from the dropdown `Custom...`, in Server type in `rtmp://localhost/live`, and for the Stream Key, enter the ID in the URL previously mentioned (for example: 6 in this case), then Start Streaming.
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,43 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+  // Just in case we don't have access to the stream, we are going to fetch it on the fly, we use the lifecycle method below to call build player again.
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  // Stops streaming video and element when navigated away
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  // We ensure that we attach the player once the video has been loaded and stream has been fetched. We could wrap the title and description in return using the Flow control logic instead or put better flow control on componentDidMount as alternative approaches.
+  buildPlayer() {
+    // Ensures the player doesn't rebuild multiple times in our lifecycle methods.
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -14,6 +47,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
